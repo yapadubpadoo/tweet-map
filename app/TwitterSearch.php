64cdf0a15ba499f603cde4fa8ebe8dd82a194a0c
yapadubpadoo <yapadubpadoo@gmail.com>
2015-11-03 02:24:51 +0700

    add search result pagination

--- app/TwitterSearch.php
+++ app/TwitterSearch.php
@@ -17,7 +17,24 @@ class TwitterSearch extends Model
             'geocode' => $lat . ',' . $lon . ',' . $miles . 'mi',
         ];
         $tweets = Twitter::getSearch($parameters);
+        if(isset($tweets->search_metadata) && isset($tweets->search_metadata->next_results)) {
+            // twitter api return newest result first, so
+            // use lowest id - 1 from current result as a max id for next request
+            $max_id = $tweets->statuses[count($tweets->statuses)-1]->id - 1;
+            $parameters = [
+                'q' => '',
+                'geocode' => $lat . ',' . $lon . ',' . $miles . 'mi',
+                'max_id' => $max_id
+            ];
+            $tweets_next_page = Twitter::getSearch($parameters);
+            $tweets->statuses = array_merge($tweets->statuses, $tweets_next_page->statuses);
+        }
         return $tweets;
+    }
+
+    public static function getNextSearchResult($lat, $lon)
+    {
+        
     }
 
     public static function convertKilometersToMiles($km)
